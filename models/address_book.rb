a0175e9e2506ce9833aaa404c4e38231d298feda
mpjr1 <mapg.jr@gmail.com>
2015-06-21 14:48:27 +0000

    Assignment 24 Completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -4,11 +4,11 @@ require "csv"
 
 class AddressBook
   attr_accessor :entries
-  
+
   def initialize
     @entries = []
   end
-  
+
   def add_entry(name, phone, email)
     index = 0
     @entries.each do |entry|
@@ -18,36 +18,49 @@ class AddressBook
       index +=1
     end
     @entries.insert(index, Entry.new(name, phone, email))
-  end 
-  
-  def import_from_csv(file_name)
-     csv_text = File.read(file_name)
-     csv = CSV.parse(csv_text, headers: true)
-    
-     csv.each do |row|
-       row_hash = row.to_hash
-       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
-     end
-      return csv.count
   end
-  
+
+  def import_from_csv(file_name)
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers: true)
+
+    csv.each do |row|
+      row_hash = row.to_hash
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+    end
+    return csv.count
+  end
+
   def binary_search(name)
     lower = 0
     upper = entries.length - 1
-    
+
     while lower <= upper
       mid = (lower + upper) / 2
       mid_name = entries[mid].name
       if name == mid_name
         return entries[mid]
-        elsif name < mid_name
+      elsif name < mid_name
         upper = mid - 1
-        elsif name > mid_name
+      elsif name > mid_name
         lower = mid + 1
       end
     end
-    
+
+    return nil
+  end
+
+  def iterative_search(name)
+    index = 0
+    while index < entries.length
+      if name == entries[index].name
+        return entries[index]
+      end
+      index += 1
+    end
     return nil
   end
   
+  
 end
+
